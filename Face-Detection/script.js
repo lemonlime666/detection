bd27a2135274781f078edd8f4c8ec27231396724
lemonlime666 <bnbwade881@gmail.com>
2020-08-27 16:23:53 +0800

    first commit

---
+++ Face-Detection/script.js
@@ -0,0 +1,35 @@
+function $id(id){ return document.getElementById(id) };
+const video = $id('video');
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+]).then(startVideo);
+
+function startVideo(){
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.log(err)
+    )
+}
+
+video.addEventListener('play', function(){
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width:video.width , height:video.height };
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async ()=>{
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+                                        .withFaceLandmarks()
+                                        .withFaceExpressions();
+        const resizeDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
+        faceapi.draw.drawDetections(canvas, resizeDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizeDetections);
+    },100)
+})
+
